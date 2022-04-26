94bb0ad8f62a4ee485e46f69e37d0f766e8afd66
Maxim Tsyganov <0xc00000f@gmail.com>
2022-04-26 20:35:30 +0300

    Initial Commit

---
+++ lbta-shimmer/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  lbta-shimmer
+//
+//  Created by Maxim Tsyganov on 26.04.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+
+        return true
+    }
+
+}
+
---
+++ lbta-shimmer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lbta-shimmer
+//
+//  Created by Maxim Tsyganov on 26.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
